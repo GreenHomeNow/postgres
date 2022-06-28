a54057e5bc9e96880ec3ba1de8fd4343bebbfb96
Akshay Devkate <devkatte.akshay98@gmail.com>
2022-06-28 15:10:57 +0200

    Production and developemnt API calls

--- client/src/apis/firmsFinder.js
+++ client/src/apis/firmsFinder.js
@@ -1,5 +1,17 @@
 import axios from 'axios';
 
+// NODE_ENV = "Development"
+
+// NODE_ENV = "Production"
+
+// IF we are in production baseurl =  /api/v1/restaurants
+
+// else baseurl = http>//localhost3001/api/v1/restaurants 
+
+
+
+const baseURL = 
+process.env.NODE_ENV === "production" ? "api/v1/firms" : "http://localhost:3006/api/v1/firms"'
 export default axios.create({
-    baseURL: "http://localhost:3006/api/v1/firms",
+    baseURL,
 });
